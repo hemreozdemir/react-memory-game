300e8af919c94bb11dce996c79680869eaf00fae
hemreozdemir <hasanemreozdemirr@gmail.com>
2022-12-21 19:43:49 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Router, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Entrance />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router> */}
+    </div>
+  );
+}
+
+export default App;
